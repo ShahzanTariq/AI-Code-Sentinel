b11963c699116af98ee345038a3a6dbd54f4c92d
ShahzanTariq <shahzantariq@gmail.com>
2025-02-27 00:13:46 -0500

    test .env

--- test.py
+++ test.py
@@ -6,9 +6,12 @@
 
 
 from google import genai
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
-client = genai.Client(api_key="AIzaSyCQFYSOtcQ4XexFAY9MBbNQY3aHZeZbpYg")
+client = genai.Client(api_key= "GOOGLE_API_KEY")
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works"
 )
