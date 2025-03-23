ec5bc02a8313d71f38f73257c9a8b2d454e28d74
ShahzanTariq <shahzantariq@gmail.com>
2025-03-23 01:55:02 -0400

    fix: observers path was parents folder instead of selected folder

--- worker_thread.py
+++ worker_thread.py
@@ -19,7 +19,7 @@ class WorkerThread(QThread):
         event_handler = ScriptChangeHandler(self.script_path, self.mainFile_path)
         event_handler.output_signal = self.output_signal  # Pass the signal to the event handler
         self.observer = Observer()
-        self.observer.schedule(event_handler, path=os.path.dirname(self.script_path), recursive=True) #Observer is gonna check script_path for creation, deletion, modification, and moving. But our event_handler (scriptchangehandler) only handles modification events.
+        self.observer.schedule(event_handler, path=self.script_path, recursive=True) #Observer is gonna check script_path for creation, deletion, modification, and moving. But our event_handler (scriptchangehandler) only handles modification events.
         self.observer.start() # Starts observing
 
         try:
